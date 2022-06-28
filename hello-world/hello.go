d06811ba2ff219ade1f2bf4de45e93ee6a1238c8
Mateus Coelho <mateuscfs@gmail.com>
2022-06-27 21:05:03 -0300

    feat: add variables and usage on Println

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -3,5 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World! Let's Go!")
+	var name string = "Mateus"
+	var version float32 = 1.1
+	var age int = 28
+	fmt.Println("Hello Mr.", name, "your age is", age)
+	fmt.Println("This program is on version", version)
 }
