524f2d45a8e9056cd73b32fc941e6c7e15071fb8
Mateus Coelho <mateuscfs@gmail.com>
2022-06-27 21:13:13 -0300

    feat: learn typeOf

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	var name string = "Mateus"
-	var version float32 = 1.1
-	var age int = 28
+	var name = "Mateus"
+	var version = 1.1
+	var age = 28
 	fmt.Println("Hello Mr.", name, "your age is", age)
 	fmt.Println("This program is on version", version)
+
+	fmt.Println("The type of the variable age is", reflect.TypeOf(age))
+	fmt.Println("The type of the variable version is", reflect.TypeOf(version))
 }
